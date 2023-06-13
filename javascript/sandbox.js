0901a4f476e0893e52dbb3c3effa51405ea05234
Parth Bindra <parthbindra26@gmail.com>
2023-06-13 16:51:19 +0530

    my first commit

---
+++ javascript/sandbox.js
@@ -0,0 +1,136 @@
+let myAge=25;
+let year =2019;
+
+console.log(age,year);
+
+age=30;
+console.log(age);
+
+// comments
+const points = 100;
+ console.log(points);
+
+var score =75;
+console.log(score);
+
+//string
+
+console.log('hello, world')
+let email= "dsdsdsdss"
+
+//string 
+let firsttname="akak";
+let lastname="";
+
+let fullname= firstname + lastname;
+console.log(fullname);
+
+//getting characters
+console.log(fullname[0]);
+
+//getting lenght
+console.log(fullname.lenght)
+
+//string methods
+console.log(fullname.toUpperCase());
+
+//commont string
+
+let email="parthbindra26@gmail.com";
+
+//slice means to move the words 
+// email.substr
+//replace('m', 'w');
+
+let result=email.lastIndexOf('a');
+console.log(result);
+
+
+
+let radius =10;
+const pi=3.14;
+console.log(radius,pi);
+//math operators %means remainder
+//order B I D M A S
+
+let result='the blog '+ likes +'likes';
+console.log(result);
+
+
+//template string way
+let reult="the blog called $(title) by$(author) has $(likes); 
+
+
+let ninjas =['ss','sssss'];
+console.log(ninjas[0]);
+
+
+//array method
+let result= ninjas.join('-');
+ninjas.indexOf();
+ninjas.concat();
+ninjas.push();
+ninjas.pop();
+
+console.log()
+
+//boolen
+// we use includes to check the true or false
+//to check the operator we use == or != >>  <<
+// typeof  to check about variable intger string or boolean
+// for loops 
+for (let i=0;i<ninjas;i++){
+    console.log('in loop:',i);
+
+}
+console.log('loop finished')
+
+conts=['ddd','ffff',fff];
+for(let i=0;i<names.lenght;i++){
+    console.log(i)
+}
+//while loops
+let i=0;
+while(i<5){
+    console.log('in loops',i);
+
+}
+// if ststeemnt 
+const age=20;
+if(age>20){
+    console.log('age is more than 20')
+}
+
+const ninjas =[ 'ssss',gggg]{
+if(ninjas.lenght>3);
+}
+else{
+
+}
+//logical not
+let iser= false;
+
+if(!user){
+
+}
+console.log(!true);
+console.log(!false);
+
+//break and contuie
+const scores=[20,28,24,23,23,32];
+for(let i=0;i<nscores.lenght;i++){
+console.log('your score',scores[i])
+
+}
+//switch 
+//break statement we use break and default 
+// varibale and block scope
+let age=30;
+if(true){
+    //inside code block we will get in the output 
+}
+//outside code block
+
+
+
+//function show scores
